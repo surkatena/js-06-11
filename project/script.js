846eb57a87391fab7d275abf5bc68a2f66fa9f36
Kolya <bazlovrabota@gmail.com>
2020-06-15 19:54:10 +0300

    added /project files
    вам спасибо, по-тихоньку вникаю в гит...

---
+++ project/script.js
@@ -0,0 +1,17 @@
+const goods = [
+    { title: 'Компьютер', price: 10000 },
+    { title: 'Мышь', price: 500 },
+    { title: 'Клавиатура', price: 1000 },
+    { title: 'Монитор', price: 7000 },
+];
+
+const getGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h2>${title}</h2><p>${price}</p></div>`;
+}
+
+const renderGoodsList = (list) => {
+    const goodsList = list.map(listItem => getGoodsItem(listItem.title, listItem.price));
+    document.querySelector('.goods-list').innerHTML = goodsList;
+}
+
+renderGoodsList(goods);
